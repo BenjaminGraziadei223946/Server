d5218f947b10eed35babb4e8eed18bcf47f0f9de
BenjaminGraziadei223946 <223946@buas.nl>
2023-11-16 14:57:51 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 2980; // Use the PORT environment variable provided by Azure or default to 3000
+
+// Define routes and middleware here
+
+// Define a basic route for testing
+app.get('/', (req, res) => {
+  res.send('Hello, Azure Web App!');
+});
+
+app.get('/api/callback', (req, res) => {
+    res.send('Getting called!');
+});
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
