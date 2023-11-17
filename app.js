6424a7f12d224fb4d045a21cb7fd1fa1244f07be
BenjaminGraziadei223946 <223946@buas.nl>
2023-11-17 09:05:54 +0100

    a

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const { BotFrameworkAdapter } = require('botbuilder');
 
 const app = express();
-const port = process.env.PORT; // Use the PORT environment variable provided by Azure or default to 3000
+const port = process.env.PORT;
+const adapter = new BotFrameworkAdapter({
+  appId: process.env.MicrosoftAppId,
+  appPassword: process.env.MicrosoftAppPassword
+});
+
+adapter.onTurnError = async (context, error) => {
+  console.error(`\n [onTurnError]: ${ error }`);
+  // Send a message to the user
+  await context.sendActivity(`Oops. Something went wrong!`);
+};
 
 // Define routes and middleware here
 app.use(morgan('dev'));
@@ -11,8 +22,26 @@ app.get('/', (req, res) => {
   res.send('Hello, Azure Web App!');
 });
 
-app.get('/api/callback', (req, res) => {
-    res.send('Getting called!');
+app.post('/api/callback', (req, res) => {
+  adapter.processActivity(req, res, async (context) => {
+      // Route to main dialog.
+      if (context.activity.type === 'conversationUpdate') {
+        // Handle conversation update logic here
+        // This is where you might detect the start of a call
+        if (context.activity.membersAdded && context.activity.membersAdded.length > 0) {
+            // Iterate over all new members added to the conversation
+            for (const member of context.activity.membersAdded) {
+                if (member.id !== context.activity.recipient.id) {
+                    // Respond to the member (user or bot) who joined the conversation
+                    await context.sendActivity('Welcome to the call!');
+                }
+            }
+        }
+    } else if (context.activity.type === 'message') {
+        // Handle messages sent during the call
+        await context.sendActivity(`You said: ${context.activity.text}`);
+    }
+  });
 });
 
 
