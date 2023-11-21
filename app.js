947d307ca8a9ac050d472a637eb5358af4b5bc57
BenjaminGraziadei223946 <223946@buas.nl>
2023-11-21 10:30:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,11 +15,12 @@ const adapter = new BotFrameworkAdapter({
 
 //const acsClient = new CommunicationIdentityClient(process.env.ACS_CONNECTION_STRING);
 
-/*const graphClient = Client.init({
+const graphClient = Client.init({
   authProvider: (done) => {
-    done(null, eyJ0eXAiOiJKV1QiLCJub25jZSI6InNXQktnUmEwME80NWgwSWxZRWhkMFZlaWlJV3VMdFdkT25qSVJBZlZJRTgiLCJhbGciOiJSUzI1NiIsIng1dCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSIsImtpZCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9jNDU4ZTU2Mi00NTRhLTRiZTktYTdkNy1lZjNkYjNlNWExZjQvIiwiaWF0IjoxNzAwMjExMDkxLCJuYmYiOjE3MDAyMTEwOTEsImV4cCI6MTcwMDI5Nzc5MSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhWQUFBQU1qd2VRL1dON3NJcWt5R0crTXdIa1RkamtPVlFuWDMyYmdUbldXdXlsb2FaWWpDa0UrajYxTkJrclNZV2VMdU9vS091UGx2d2hFVEg0cDNxbmFmKzFCTmZWclBnSU9qQk1CSTBKVUJIVVBJPSIsImFtciI6WyJwd2QiLCJyc2EiLCJtZmEiXSwiYXBwX2Rpc3BsYXluYW1lIjoiR3JhcGggRXhwbG9yZXIiLCJhcHBpZCI6ImRlOGJjOGI1LWQ5ZjktNDhiMS1hOGFkLWI3NDhkYTcyNTA2NCIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiZTNiMTAwYWYtYWE1MS00MGU1LTlmYjUtODVkNTIwYWY1ZTAzIiwiZmFtaWx5X25hbWUiOiJHcmF6aWFkZWkiLCJnaXZlbl9uYW1lIjoiQmVuamFtaW4iLCJpZHR5cCI6InVzZXIiLCJpcGFkZHIiOiIxNDUuMTI4LjI0NS4zMyIsIm5hbWUiOiJCZW5qYW1pbiBHcmF6aWFkZWkgfCBNYXhhcm8iLCJvaWQiOiI3ZjA3ZDM2Zi1jOGUzLTQ0OGYtOTA3MS1jNzQ0NDZhMmE4MWIiLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtMjI5NjgwNTE0OS0zODM4MjQ4NjYwLTI3NDA1NzM4OTYtMjg2NTIiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDJFNTUyQjU5QiIsInJoIjoiMC5BVEVBWXVWWXhFcEY2VXVuMS04OXMtV2g5QU1BQUFBQUFBQUF3QUFBQUFBQUFBRGpBTWcuIiwic2NwIjoib3BlbmlkIHByb2ZpbGUgVXNlci5SZWFkIGVtYWlsIiwic2lnbmluX3N0YXRlIjpbImR2Y19tbmdkIiwiZHZjX2NtcCIsImR2Y19kbWpkIiwiaW5rbm93bm50d2siLCJrbXNpIl0sInN1YiI6Ims1TXdlVEVPQklheXZWRzZuekZYUWlDV29FeUE0MmZncUJLNHJRVEl1bU0iLCJ0ZW5hbnRfcmVnaW9uX3Njb3BlIjoiRVUiLCJ0aWQiOiJjNDU4ZTU2Mi00NTRhLTRiZTktYTdkNy1lZjNkYjNlNWExZjQiLCJ1bmlxdWVfbmFtZSI6ImJncmF6aWFkZWlAbWF4YXJvLm5sIiwidXBuIjoiYmdyYXppYWRlaUBtYXhhcm8ubmwiLCJ1dGkiOiJ0THZhZGt3TjJFQ05RNlNsdlRJX0FBIiwidmVyIjoiMS4wIiwid2lkcyI6WyI2MmU5MDM5NC02OWY1LTQyMzctOTE5MC0wMTIxNzcxNDVlMTAiLCJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX2NjIjpbIkNQMSJdLCJ4bXNfc3NtIjoiMSIsInhtc19zdCI6eyJzdWIiOiIxUUM1M2c3TFZSM0JmTFVDbmNqQWlZSFRaYnB4b0QxdTcta1RkazBXT3k4In0sInhtc190Y2R0IjoxNTEzNTkxODIwLCJ4bXNfdGRiciI6IkVVIn0.SoaazDMVhD2oXYsYQTfweZUtfksI37cOhYMoyeUC7mNYpNBvaqMIT5hpNML2a45Z7FB3Fo9GF2JI6DHhcGSyuHhXZM1HmH6hO9N9dLlerDToDlpw5MqNNZBidxJdYADUfSzvJBAVxTwPnlKKMSRD9rTGkTiHQEUBPVtBNWnDuTHlMPtF-FgHZaYyXYZ3p57zwEsf1Z3jY1KTk-OM_f1oZSAx_dT8NNY1GHz13evcSK6NVokGiawp1qNbMrlcdYLbUozkFgQ0By0ekB7WjnQ64lOmmFWLvFZiNRH6_Uhy9xp88aXE3P1AdDISC0ayM5OxEZV6fdlSfU0m-RqIlf2oew); // Provide the token
+    const token = process.env.GraphAccessToken;
+    done(null, token); // Provide the token
   }
-});*/
+});
 
 adapter.onTurnError = async (context, error) => {
   console.error(`\n [onTurnError]: ${ error }`);
@@ -42,7 +43,7 @@ app.post('/api/messages', (req, res) => {
 app.post('/api/callback', async (req, res) => {
   const callId = req.body.callId; // Extract call ID from the request
   try {
-    await answerCall(callId, eyJ0eXAiOiJKV1QiLCJub25jZSI6InNXQktnUmEwME80NWgwSWxZRWhkMFZlaWlJV3VMdFdkT25qSVJBZlZJRTgiLCJhbGciOiJSUzI1NiIsIng1dCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSIsImtpZCI6IjlHbW55RlBraGMzaE91UjIybXZTdmduTG83WSJ9.eyJhdWQiOiIwMDAwMDAwMy0wMDAwLTAwMDAtYzAwMC0wMDAwMDAwMDAwMDAiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC9jNDU4ZTU2Mi00NTRhLTRiZTktYTdkNy1lZjNkYjNlNWExZjQvIiwiaWF0IjoxNzAwMjExMDkxLCJuYmYiOjE3MDAyMTEwOTEsImV4cCI6MTcwMDI5Nzc5MSwiYWNjdCI6MCwiYWNyIjoiMSIsImFpbyI6IkFWUUFxLzhWQUFBQU1qd2VRL1dON3NJcWt5R0crTXdIa1RkamtPVlFuWDMyYmdUbldXdXlsb2FaWWpDa0UrajYxTkJrclNZV2VMdU9vS091UGx2d2hFVEg0cDNxbmFmKzFCTmZWclBnSU9qQk1CSTBKVUJIVVBJPSIsImFtciI6WyJwd2QiLCJyc2EiLCJtZmEiXSwiYXBwX2Rpc3BsYXluYW1lIjoiR3JhcGggRXhwbG9yZXIiLCJhcHBpZCI6ImRlOGJjOGI1LWQ5ZjktNDhiMS1hOGFkLWI3NDhkYTcyNTA2NCIsImFwcGlkYWNyIjoiMCIsImRldmljZWlkIjoiZTNiMTAwYWYtYWE1MS00MGU1LTlmYjUtODVkNTIwYWY1ZTAzIiwiZmFtaWx5X25hbWUiOiJHcmF6aWFkZWkiLCJnaXZlbl9uYW1lIjoiQmVuamFtaW4iLCJpZHR5cCI6InVzZXIiLCJpcGFkZHIiOiIxNDUuMTI4LjI0NS4zMyIsIm5hbWUiOiJCZW5qYW1pbiBHcmF6aWFkZWkgfCBNYXhhcm8iLCJvaWQiOiI3ZjA3ZDM2Zi1jOGUzLTQ0OGYtOTA3MS1jNzQ0NDZhMmE4MWIiLCJvbnByZW1fc2lkIjoiUy0xLTUtMjEtMjI5NjgwNTE0OS0zODM4MjQ4NjYwLTI3NDA1NzM4OTYtMjg2NTIiLCJwbGF0ZiI6IjMiLCJwdWlkIjoiMTAwMzIwMDJFNTUyQjU5QiIsInJoIjoiMC5BVEVBWXVWWXhFcEY2VXVuMS04OXMtV2g5QU1BQUFBQUFBQUF3QUFBQUFBQUFBRGpBTWcuIiwic2NwIjoib3BlbmlkIHByb2ZpbGUgVXNlci5SZWFkIGVtYWlsIiwic2lnbmluX3N0YXRlIjpbImR2Y19tbmdkIiwiZHZjX2NtcCIsImR2Y19kbWpkIiwiaW5rbm93bm50d2siLCJrbXNpIl0sInN1YiI6Ims1TXdlVEVPQklheXZWRzZuekZYUWlDV29FeUE0MmZncUJLNHJRVEl1bU0iLCJ0ZW5hbnRfcmVnaW9uX3Njb3BlIjoiRVUiLCJ0aWQiOiJjNDU4ZTU2Mi00NTRhLTRiZTktYTdkNy1lZjNkYjNlNWExZjQiLCJ1bmlxdWVfbmFtZSI6ImJncmF6aWFkZWlAbWF4YXJvLm5sIiwidXBuIjoiYmdyYXppYWRlaUBtYXhhcm8ubmwiLCJ1dGkiOiJ0THZhZGt3TjJFQ05RNlNsdlRJX0FBIiwidmVyIjoiMS4wIiwid2lkcyI6WyI2MmU5MDM5NC02OWY1LTQyMzctOTE5MC0wMTIxNzcxNDVlMTAiLCJiNzlmYmY0ZC0zZWY5LTQ2ODktODE0My03NmIxOTRlODU1MDkiXSwieG1zX2NjIjpbIkNQMSJdLCJ4bXNfc3NtIjoiMSIsInhtc19zdCI6eyJzdWIiOiIxUUM1M2c3TFZSM0JmTFVDbmNqQWlZSFRaYnB4b0QxdTcta1RkazBXT3k4In0sInhtc190Y2R0IjoxNTEzNTkxODIwLCJ4bXNfdGRiciI6IkVVIn0.SoaazDMVhD2oXYsYQTfweZUtfksI37cOhYMoyeUC7mNYpNBvaqMIT5hpNML2a45Z7FB3Fo9GF2JI6DHhcGSyuHhXZM1HmH6hO9N9dLlerDToDlpw5MqNNZBidxJdYADUfSzvJBAVxTwPnlKKMSRD9rTGkTiHQEUBPVtBNWnDuTHlMPtF-FgHZaYyXYZ3p57zwEsf1Z3jY1KTk-OM_f1oZSAx_dT8NNY1GHz13evcSK6NVokGiawp1qNbMrlcdYLbUozkFgQ0By0ekB7WjnQ64lOmmFWLvFZiNRH6_Uhy9xp88aXE3P1AdDISC0ayM5OxEZV6fdlSfU0m-RqIlf2oew);      // Answer the call using Graph AP
+    await answerCall(callId);      // Answer the call using Graph AP
     res.status(200).send('Call handled');
   } catch (error) {
     console.error('Error handling call:', error);
@@ -50,32 +51,35 @@ app.post('/api/callback', async (req, res) => {
   }
 });
 
-async function answerCall(callId, accessToken) {
-  const graphApiEndpoint = `https://graph.microsoft.com/v1.0/communications/calls/${callId}/answer`;
+async function answerCall(callId) {
+  const graphApiEndpoint = `communications/calls/${callId}/answer`;
 
-  const headers = {
-    'Authorization': `Bearer ${accessToken}`,
-    'Content-Type': 'application/json'
-  };
-
-  const body = {
-    // Define the callback URL and other necessary parameters as per your setup
-    callbackUri: 'https://conversbotserver.azurewebsites.net/api/callback',
-    acceptedModalities: ['audio'],
-    mediaConfig: {
-      '@odata.type': '#microsoft.graph.serviceHostedMediaConfig'
+  const requestParameters = {
+    method: 'POST',
+    url: graphApiEndpoint,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: {
+      callbackUri: 'https://conversbotserver.azurewebsites.net/api/callback',
+      acceptedModalities: ['audio'],
+      mediaConfig: {
+        '@odata.type': '#microsoft.graph.serviceHostedMediaConfig'
+      }
     }
   };
 
   try {
-    const response = await axios.post(graphApiEndpoint, body, { headers: headers });
-    console.log('Call answered:', response.data);
-    return response.data;
+    const response = await graphClient.api(requestParameters.url)
+                                     .post(requestParameters.body);
+    console.log('Call answered:', response);
+    return response;
   } catch (error) {
     console.error('Error answering call:', error);
     throw error;
   }
 }
+
 
 
 async function handleRealTimeMedia(callId) {
