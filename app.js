834e6f966bf018f04e2af8e80109d9b4b23e2875
BenjaminGraziadei223946 <223946@buas.nl>
2023-11-23 12:00:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.post('/api/calling', async (req, res) => {
 });
 
 app.post('/api/callback', async (req, res) => {
-  const callId = req.body.callId; // Extract call ID from the request
+  const callId = req.body.id; // Extract call ID from the request
   const body = req.body;
   appInsights.defaultClient.trackTrace({ message: 'HTTP request recieved', properties: { body } });
   try {
